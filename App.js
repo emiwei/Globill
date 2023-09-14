1662b0c371f762efb984cc24c50300d6269994c6
emiwei <95663540+emiwei@users.noreply.github.com>
2023-09-14 03:25:36 +0200

    Created pages and linked them using navigation

--- App.js
+++ App.js
@@ -1,20 +1,84 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+function HomeScreen({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'flex-start', justifyContent: 'flex-start', padding: 20, paddingTop: 110 }}>
+      <Pressable onPress={() => navigation.navigate('NewBill')}>
+        <Text style={styles.categories}>New Bill</Text>
+      </Pressable>
+      <Pressable onPress={() => navigation.navigate('JoinBill')}>
+        <Text style={styles.categories}>Join Bill</Text>
+      </Pressable>
+      <Pressable onPress={() => navigation.navigate('MyBills')}>
+        <Text style={styles.categories}>My Bills</Text>
+      </Pressable>
+      <Pressable onPress={() => navigation.navigate('LogIn')}>
+        <Text style={styles.categories}>Log In/Sign Up</Text>
+      </Pressable>
     </View>
   );
 }
 
+function NewBill() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Create a new bill</Text>
+    </View>
+  );
+}
+
+function JoinBill() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Join an existing bill</Text>
+    </View>
+  );
+}
+
+function MyBill() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Previous bills</Text>
+    </View>
+  );
+}
+
+function LogIn() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Log in or create an account</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+          cardStyle: { backgroundColor: '#363738', }
+        }}>
+        <Stack.Screen name="Home" component={HomeScreen}/>
+        <Stack.Screen name="NewBill" component={NewBill} />
+        <Stack.Screen name="JoinBill" component={JoinBill} />
+        <Stack.Screen name="MyBills" component={MyBill} />
+        <Stack.Screen name="LogIn" component={LogIn} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    
+  );
+}
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  categories: {
+    fontSize: 37,
+    paddingBottom: 40,
+    fontFamily: 'Avenir'
+  }
 });
